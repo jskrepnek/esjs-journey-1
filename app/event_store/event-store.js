23e4585204b83e5932283d6d910d02f6d668e09c
Joel Skrepnek <jskrepnek@gmail.com>
2015-03-29 19:14:24 -0700

    refactor event store

--- app/event_store/event-store.js
+++ app/event_store/event-store.js
@@ -1,91 +1,95 @@
 angular.module('eventStore', [])
 
-.factory('guid', function() {
-    return {
-        new : function () {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-                return v.toString(16);
-            });
-        }
-    }
-})
-
-.factory('EventStore', function ($http, $q, $timeout, guid) {
-
-    var url = 'http://127.0.0.1:2113/streams/';
-
-    processEntries = function (entries, onEntry) {
-        var getEntryPromises = [];
-        entries.reverse().forEach(function (entry) {
-            var entryUri = null;
-            entry.links.forEach(function (link) {
-                if (link.relation === "alternate") {
-                    entryUri = link.uri;
-                }
-            });
-            var entryPromise = $http({
-                method: 'GET',
-                url: entryUri,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }).success(function (data) {
-                onEntry(data);
-            });
-            getEntryPromises.push(entryPromise);
-        });
-        return getEntryPromises;
-    };
-
-    processPrevUri = function (prevUri, onEvent) {
-        $http({
-            method: 'GET',
-            url: prevUri,
-            headers: {
-                'Content-Type': 'vnd.eventstore.atom+json'
+    .factory('guid', function () {
+        return {
+            new: function () {
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                    return v.toString(16);
+                });
             }
-        }).success(function (data) {
-            $q.all(processEntries(data.entries, onEvent)).then(function () {
-                var previous = null;
-                data.links.forEach(function (link) {
-                    if (link.relation === "previous") {
-                        previous = link.uri;
+        }
+    })
+
+    .factory('EventStore', function ($http, $q, $timeout, guid) {
+
+        var url = 'http://127.0.0.1:2113/streams/';
+
+        processEntries = function (entries, onEntry) {
+            var getEntryPromises = [];
+
+            entries.reverse().forEach(function (entry) {
+                var entryUri = null;
+
+                entry.links.forEach(function (link) {
+                    if (link.relation === "alternate") {
+                        entryUri = link.uri;
                     }
                 });
-                if (previous) {
-                    processPrevUri(previous, onEvent);
-                } else {
-                    $timeout(function () {
-                        processPrevUri(prevUri, onEvent);
-                    }, 3000);
-                }
+
+                var entryPromise =
+                    $http
+                        .get(entryUri)
+                        .success(function (data) {
+                            onEntry(data);
+                        });
+
+                getEntryPromises.push(entryPromise);
             });
-        });
-    };
+            return $q.all(getEntryPromises);
+        };
 
-    var exports = {
+        processPrevUri = function (prevUri, onEvent) {
+            $http
+                .get(prevUri, {
+                    headers: {
+                        'Content-Type': 'vnd.eventstore.atom+json'
+                    }
+                })
+                .success(function (data) {
+                    processEntries(data.entries, onEvent)
+                        .then(function () {
+                            var previousLinkUri = null;
 
-        appendToStream : function (streamName, eventType, eventData) {
-            $http({
-                method: 'POST',
-                url: url + streamName,
-                data: [{
+                            data.links.forEach(function (link) {
+                                if (link.relation === "previous") {
+                                    previousLinkUri = link.uri;
+                                }
+                            });
+
+                            if (previousLinkUri) {
+                                processPrevUri(previousLinkUri, onEvent);
+                            } else {
+                                $timeout(function () {
+                                    processPrevUri(prevUri, onEvent);
+                                }, 3000);
+                            }
+                        });
+                });
+        };
+
+        var exports = {
+
+            appendToStream: function (streamName, eventType, eventData) {
+                var streamUrl = url + streamName;
+                var data = [{
                     eventId: guid.new(),
                     eventType: eventType,
                     data: eventData
-                }],
-                headers: {
-                    'Content-Type': 'application/vnd.eventstore.events+json'
-                }
-            })
-        },
+                }];
 
-        subscribeToStream : function (streamName, onEvent) {
-            processPrevUri(url + streamName, onEvent);
-        }
-    };
+                $http.post(streamUrl, data, {
+                    headers: {
+                        'Content-Type': 'application/vnd.eventstore.events+json'
+                    }
+                })
+            },
 
-    return exports;
-});
+            subscribeToStream: function (streamName, onEvent) {
+                processPrevUri(url + streamName, onEvent);
+            }
+        };
+
+        return exports;
+    });
 
